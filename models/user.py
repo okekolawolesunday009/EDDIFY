d696ceff248240a2fee2908f0f9fda8c0c346736
Timishot <timidotcom58@gmail.com>
2024-01-29 21:31:45 +0100

    upadte user

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""holds class User"""
+
+import models
+from models.base_model import BaseModel
+
+class User(BaseModel):
+	"""Representation of a user"""
+	first_name = ""
+	last_name = ""
+	email = ""
+	image_file = ""
+	password = ""
+	confirmed = ""
+	phoneNo= ""
+	country = ""
+	courses = ""
+	confirmed = False
+	
