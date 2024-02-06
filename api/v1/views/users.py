ede9aa5d470668982a083463c2bc2e7a75718c1c
Timishot <timidotcom58@gmail.com>
2024-02-06 21:00:15 +0100

    api for user fix

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,7 +6,9 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/user/all_users.yml')
 def get_users():
     """
     Retrieves the list of all user objects
@@ -15,29 +17,32 @@ def get_users():
     all_users = storage.all(User).values()
     list_users = []
     for user in all_users:
-        list.append(user.to_dict())
+        list_users.append(user.to_dict())
     return jsonify(list_users)
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/user/get_user.yml', methods=['GET'])
 def get_user(user_id):
-    """retrieve a user """
+    """ Retrieves an user """
     user = storage.get(User, user_id)
     if not user:
         abort(404)
 
     return jsonify(user.to_dict())
 
+
 @app_views.route('/users/<user_id>', methods=['DELETE'],
                  strict_slashes=False)
+@swag_from('documentation/user/delete_user.yml', methods=['DELETE'])
 def delete_user(user_id):
     """
-    Delete a user object
+    Deletes a user Object
     """
 
-    user = storage.get(user, user_id)
+    user = storage.get(User, user_id)
 
-    if not User:
+    if not user:
         abort(404)
 
     storage.delete(user)
@@ -45,13 +50,15 @@ def delete_user(user_id):
 
     return make_response(jsonify({}), 200)
 
+
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/user/post_user.yml', methods=['POST'])
 def post_user():
     """
-    creates a user
+    Creates a user
     """
     if not request.get_json():
-        abort(400, description="Not a Json")
+        abort(400, description="Not a JSON")
 
     if 'email' not in request.get_json():
         abort(400, description="Missing email")
@@ -63,7 +70,9 @@ def post_user():
     instance.save()
     return make_response(jsonify(instance.to_dict()), 201)
 
+
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/user/put_user.yml', methods=['PUT'])
 def put_user(user_id):
     """
     Updates a user
